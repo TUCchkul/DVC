0a8696ebffac9797fc6a63e7368385dd35c94fa1
kul <kirticse.chakma869@gmail.com>
2022-01-28 10:15:22 +0100

    stage_03 update

--- src/stage_03_train.py
+++ src/stage_03_train.py
@@ -4,6 +4,7 @@ import pandas as pd
 import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import ElasticNet
+import joblib
 
 def train(config_path, params_path):
     config = read_yaml(config_path)
@@ -11,20 +12,37 @@ def train(config_path, params_path):
 
     artifacts_dir = config["artifacts"]['artifacts_dir']
     split_data_dir = config["artifacts"]["split_data_dir"]
+
+
     train_data_filename = config["artifacts"]["train"]
+
+
     train_data_path = os.path.join(artifacts_dir, split_data_dir, train_data_filename)
-    train_data=pd.read_csv(train_data_path)
-    train_x=train_data.drop("quality", axis=1)
-    train_y=train_data["quality"]
-    alpha=params["model_params"]["ElasticNet"]["alpha"]
-    l1_ratio=params["model_params"]["ElasticNet"]["l1_ratio"]
-    random_state=params["base"]["random_state"]
+    
+    train_data = pd.read_csv(train_data_path)
+
+    train_y = train_data["quality"]
+    train_x = train_data.drop("quality", axis=1)
+
+    alpha = params["model_params"]["ElasticNet"]["alpha"]
+    l1_ratio = params["model_params"]["ElasticNet"]["l1_ratio"]
+    random_state = params["base"]["random_state"]
 
 
+    lr = ElasticNet(alpha=alpha, l1_ratio=l1_ratio, random_state=random_state)
+    lr.fit(train_x, train_y)
 
-    lr=ElasticNet(alpha=alpha,l1_ratio=l1_ratio,random_state=random_state)
-    lr.fit(train_x,train_y)
-    print("done")
+    model_dir = config["artifacts"]["model_dir"]
+    model_filename = config["artifacts"]["model_filename"]
+
+    model_dir = os.path.join(artifacts_dir, model_dir)
+
+    create_directory([model_dir])
+
+    model_path = os.path.join(model_dir, model_filename)
+
+
+    joblib.dump(lr, model_path)
 
 if __name__ == '__main__':
     args = argparse.ArgumentParser()
